b4135660935af2ce18ac42a612d3c9f5d41727c8
digirak <raknath@gmail.com>
2020-06-25 11:22:21 +0200

    Init script

---
+++ __init__.py
@@ -0,0 +1,21 @@
+""" Contains all the import statements """
+"""Basic set of import statements of necessary 
+    imports"""
+
+import numpy as np
+import vip_hci as vip
+import pandas as pd
+from astropy.io import fits
+from matplotlib import pyplot as plt
+#############
+#Rakesh's imports go in this space
+#############
+from scipy.signal import savgol_filter
+
+#############
+
+##########
+#Malavika's import statements go in this space
+##########
+
+##########
