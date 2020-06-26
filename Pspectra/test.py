ec06fdbc6e43ef4469c6c968b4a7716ae541ce6c
Malavika Vasist <malavika.vasist47@gmail.com>
2020-06-26 14:05:14 +0200

    last changes

--- Pspectra/test.py
+++ Pspectra/test.py
@@ -5,8 +5,8 @@ from Pspectra.retrieveSpectrum.retrieveSpectrum import retrieve_spectrum
 data_filename = 'sample_file.json' 
 elements =[] 
 
-Spec = retrieve_spectrum(data_folder='Data/', data_filename= data_filename, \
+Spec = retrieve_spectrum(data_folder='./', data_filename= data_filename, \
         N_temp= 1 , state_T= 'None' ,  \
         elements= elements, N_ab=1, state_ab='None',\
-        spectrum_folder ='/home/malavika/Documents/petitRT/spectrum/',\
+        spectrum_folder ='/home/malavika/Documents/petitRT/spectrum_codeastro/',\
         delta = 20 , wlmin = 0.3 , wlmax= 15)
