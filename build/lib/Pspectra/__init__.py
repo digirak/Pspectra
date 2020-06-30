610337552f3c856d38c4ecfb935a693ec109a233
Malavika Vasist <malavika.vasist47@gmail.com>
2020-06-30 22:20:15 +0200

    mode

--- build/lib/Pspectra/__init__.py
+++ build/lib/Pspectra/__init__.py
@@ -1,1 +1,13 @@
-kesjflj
+""" Contains all the import statements """
+"""Basic set of import statements of necessary 
+    imports"""
+
+__all__ = ["CrossCorr", "retrieveSpec"]
+
+#############
+from .CCF import CrossCorr
+
+#############
+from .retrieveSpectrum import retrieveSpec
+##########
+
--- build/lib/Pspectra/test.py
+++ build/lib/Pspectra/test.py
@@ -11,7 +11,7 @@ Spectra = retrieveSpec(data_folder='Pspectra/', data_filename= data_filename, \
         N_temp= 1 , state_T= 'None' ,  \
         elements= elements, N_ab=1, state_ab='None',\
         spectrum_folder ='/home/malavika/Documents/petitRT/spectrum_codeastro/',\
-        delta = 20 , wlmin = 0.3 , wlmax= 15, mode = 'lbl') 
+        delta = 20 , wlmin = 0.3 , wlmax= 15) 
 
 s= Spectra()[0]
 
